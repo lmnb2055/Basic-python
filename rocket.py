1d12aacd554a3707f96a3117db8d6e9f09494beb
Sidney <111455634+lmnb2055@users.noreply.github.com>
2023-09-06 22:43:53 +0800

    Add files via upload

---
+++ rocket.py
@@ -0,0 +1,28 @@
+"""
+File: rocket.py
+Name:
+-----------------------
+This program should implement a console program
+that draws ASCII art - a rocket.
+The size of rocket is determined by a constant
+defined as SIZE at top of the file.
+Output format should match what is shown in the sample
+run in the Assignment 2 Handout.
+
+"""
+
+# This constant determines rocket size.
+SIZE = 3
+
+
+def main():
+    """
+    TODO:
+    """
+    pass
+
+
+# DO NOT EDIT CODE BELOW THIS LINE #
+
+if __name__ == "__main__":
+    main()
